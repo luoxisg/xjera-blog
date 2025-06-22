eeb1dc303035823015608f65739e230e7a6d4625
luoxisg <49471150+luoxisg@users.noreply.github.com>
2025-06-22 21:33:33 +0800

    Update blogRoutes.js

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,25 +1,59 @@
 const express = require('express');
 const Blog = require('../models/blog');
+
 const router = express.Router();
+
+// Blog index - display all blogs
 router.get('/', (req, res) => {
-  Blog.find().sort({ createdAt: -1 })
-    .then(result => res.render('index', { blogs: result, title: '所有博客' }))
-    .catch(err => console.log(err));
+  Blog.find().sort({ createdAt: -1 }) // sort by creation time descending
+    .then(result => {
+      res.render('index', { blogs: result, title: 'All Blogs' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
-router.get('/create', (req, res) => res.render('create', { title: '创建一篇新博客' }));
+
+// Redirect to the create new blog page
+router.get('/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+});
+
+// View a single blog post
 router.get('/:id', (req, res) => {
-  Blog.findById(req.params.id)
-    .then(result => res.render('details', { blog: result, title: '博客详情' }))
-    .catch(() => res.status(404).render('404', { title: '博客未找到' }));
+  const id = req.params.id;
+  Blog.findById(id)
+    .then(result => {
+      res.render('details', { blog: result, title: 'Blog Details' });
+    })
+    .catch(err => {
+        res.status(404).render('404', { title: 'Blog not found' });
+    });
 });
+
+// Create a new blog (handle POST request)
 router.post('/', (req, res) => {
-  new Blog(req.body).save()
-    .then(() => res.redirect('/blogs'))
-    .catch(err => console.log(err));
+  const blog = new Blog(req.body);
+  blog.save()
+    .then(result => {
+      res.redirect('/blogs');
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
+
+// Delete a blog
 router.delete('/:id', (req, res) => {
-  Blog.findByIdAndDelete(req.params.id)
-    .then(() => res.json({ redirect: '/blogs' }))
-    .catch(err => console.log(err));
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      // AJAX request expects a JSON response
+      res.json({ redirect: '/blogs' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
+
 module.exports = router;
